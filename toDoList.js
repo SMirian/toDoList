9ae780abbd9e61e9471f5846cf94bd80a762f041
SMirian <smirian.ramos@gmail.com>
2022-01-26 01:26:37 -0500

    rename code

--- toDoList.js
+++ toDoList.js
@@ -1,30 +1,70 @@
 var app = new Vue({
-    el: '#app',
-    data: {
-       arraywords: [],
-       newword: ''
-    },
- 
-   
-    methods:{
-      save(){
-      this.arraywords.push({
-        add: this.newword
-      })
-         
-      },
-      edit(index){
-        this.arrayword[index].estado = true
-
-        // console.log("edit",index)
-      },
-    
-      remove(index){
-      this.arraywords.splice(index,1)
+  el: "#app",
+  data: {
+    toDoList: [
+       {
+         text: 'xxx',
+         id: '1'
        },
+       {
+         text: 'tes',
+         id: '4'
+       },
+       {
+         text: 'ff',
+         id: '5'
+       }
+    ],
+    newword: "",
 
-      removeall(index){
-        this.arraywords.splice(index)
-         }
-    }
-  })
+    formToDo: {
+      id: null,
+      text: "",
+    },
+  },
+
+  methods: {
+    saveToDo() {
+      if (!!this.formToDo.id) {
+        /*
+          https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array/findIndex
+        */
+        const toDoIndex = this.toDoList.findIndex(
+          (toDo) => toDo.id === this.formToDo.id
+        );
+
+        this.toDoList[toDoIndex].text = this.formToDo.text;
+
+        this.cleanFormToDo();
+      } else {
+        this.addToDo();
+      }
+    },
+
+    addToDo() {
+      this.toDoList.push({
+        text: this.formToDo.text,
+        id: this.toDoList + 1,
+      });
+      this.cleanFormToDo();
+    },
+
+    editToDoButtonClick(toDo) {
+      this.formToDo.text = toDo.text;
+      this.formToDo.id = toDo.id;
+    },
+
+    remove(index) {
+      this.toDoList.splice(index, 1);
+    },
+
+    removeall() {
+      this.toDoList = [];
+    },
+
+    cleanFormToDo() {
+      this.formToDo.text = "";
+      this.formToDo.id = null;
+    },
+  },
+});
